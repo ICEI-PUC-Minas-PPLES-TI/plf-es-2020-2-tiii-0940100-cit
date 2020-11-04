2e7e1ed5996b8685d5630def3dae407421bc4e0e
José Maurício Guimarães França <josemauriciogf@gmail.com>
2020-11-04 19:44:24 -0300

    O, P

---
+++ Artefatos/comandosSGBD/O.sql
@@ -0,0 +1,6 @@
+USE cit;
+INSERT INTO denuncia_contribuicao
+(descricao, criado_em, anonimo, denuncia_id, cidadao_id)
+VALUES ('buraco', '15-03-2020', 0, 1, 1);
+
+SELECT * from denuncia_contribuicao;
---
+++ Artefatos/comandosSGBD/P.sql
@@ -0,0 +1,6 @@
+USE cit;
+INSERT INTO denuncia_contribuicao
+(descricao, criado_em, anonimo, denuncia_id, organizacao_usuario_id)
+VALUES ('buraco tampado', '20-03-2020', 0, 1, 2);
+
+SELECT * from denuncia_contribuicao;
