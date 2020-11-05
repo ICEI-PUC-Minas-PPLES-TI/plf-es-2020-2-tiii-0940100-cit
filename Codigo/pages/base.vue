b0a49c579cd8f0f93d4233ffbdcd9d78d6318574
Lucas Angelo <lcs2001_@hotmail.com>
2020-11-05 01:12:18 -0300

    index preliminar

--- Codigo/pages/base.vue
+++ Codigo/pages/base.vue
@@ -1,9 +1,69 @@
 <template>
-    <div>
-        <p class="classe">
-            Isso é um arquivo para se utilizar de copia para os outros
-        </p>
+   
+    <div class="container-fluid">
+        
+        <div class="row">
+
+            <div class="col-12">
+
+                <div class="row">
+                    <div class="col-12">
+                        <p id="auth" ><span>Login</span> | <span>Registro</span></p>
+                    </div>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-6">
+                        
+                        <div class="row">
+                            <div class="col-12">
+                                <div id="logo-text">
+                                    <img class="d-inline" src="@/assets/img/citlogo.svg" width="128px" height="128px" alt="">
+                                    <h1 class="d-inline">CIT</h1>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <p id="slogan">Todos juntos<br> por cidades<br> melhores!</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-6">
+                            <img id="home-image" src="@/assets/img/home-image.png" width="100%" height="450px" alt="home image">
+                    </div>
+
+                </div>
+
+                <br>
+                <br>
+                <br>
+
+                <div class="row">
+
+                    <div class="col-6">
+                        <p class="d-block text-center">
+                            <button class="btn denunciar">Denunciar</button>
+                            <button class="btn verDenuncias">Ver denúncias</button>
+                        </p>
+                    </div>
+
+                    <div class="col-6">
+                        <p class="text-center">Total de 200 denúncias já efetuadas!</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
     </div>
+   
 </template>
 
 <script>
@@ -13,9 +73,62 @@ export default {
 </script>
 
 <style scoped>
-    .classe{
-        color: red;
-        text-align: center;
-        font-size: 35pt;
+
+    * {
+        margin: 0;
+        padding: 0;
+        background-color: #264445;
     }
+
+    .container-fluid{
+        color: #ffffff;
+        height: 100vh;
+        width: 100vw;
+    }
+
+    #auth{
+        font-family: 'Roboto', sans-serif;
+        padding-top: 1.5vh;
+        padding-right: 2.5vw;
+        text-align: right;
+    }
+
+    #home-image{
+        margin-top: 6vh;
+        padding-right: 5vw;
+        padding-left: 2vw;
+    }
+
+    #logo-text{
+        font-family: 'Tillana', cursive;
+        margin-top: 6vh;
+        padding-left: 10vw;
+        padding-right: 2vw;
+    }
+
+    #slogan{
+        margin-left: 10vw;
+        font-size: 3.25rem;
+        font-family: 'Rum Raisin', sans-serif;
+    }
+
+    .denunciar, .verDenuncias{
+        font-family: 'Roboto', sans-serif;
+        color: white;
+        padding: 30px 50px;
+    }
+
+    .denunciar {
+        margin-right: 80px;
+        background: #38AE90;
+    }
+
+    .verDenuncias {
+        margin-left: 80px;
+        background: #7EE34D;
+    }
+
+
+
+
 </style>
