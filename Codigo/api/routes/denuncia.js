02d6ecc4a2a0c021cd54deb253d57e85b4be36f6
Guizombas <guilherme.gabriel.532@gmail.com>
2020-11-17 00:19:47 -0300

    back e front pagina de mapa

--- Codigo/api/routes/denuncia.js
+++ Codigo/api/routes/denuncia.js
@@ -14,8 +14,30 @@ router.get('/todasDenuncias', async (req, res) => {
     const connection = await db.connect();
 
     // Retorna o error se houver, se não retorna o resultado
-    connection.query( query, (error, results, fields) => error?res.json(error):res.json(results))
-    connection.end();
+    await connection.query( query, (error, results, fields) => error?res.json(error):res.json(results))
+    await connection.end();
+})
+
+router.get('/denunciasProximas/:lat/:lng', async (req, res) => {
+
+    const lat = req.params.lat
+    const lng = req.params.lng
+    const query =
+    `SELECT d.id, d.latitude, d.longitude, d.logradouro, c.descricao, dcf.url
+     FROM denuncia d
+     INNER JOIN denuncia_has_categoria dhc on d.id = dhc.denuncia_id
+     INNER JOIN categoria c ON dhc.categoria_id = c.id
+     INNER JOIN denuncia_contribuicao dc ON d.id = dc.denuncia_id
+     LEFT  JOIN denuncia_contribuicao_foto dcf ON dc.id = dcf.denuncia_contribuicao_id
+     WHERE latitude BETWEEN (${lat}-0.015) AND (${lat}+0.0015)
+     AND longitude BETWEEN (${lng}-0.015) AND (${lng}+0.0015); `
+
+    const db = new Database();
+    const connection = await db.connect();
+
+    await connection.query(query, (error, results, fields) => error?res.json(error):res.json(results))
+    await connection.end()
+
 })
 
 module.exports = router
